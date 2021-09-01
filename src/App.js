fd6b3831ce384141ed8b5300984981777b273a0c
octo <dlqhdgkr456@gmail.com>
2021-09-01 21:54:26 +0900

    2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Potato from './Potato'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!</h1>
+      <Potato />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+// <App /> => component의 한 종류, react는 component와 함께 동작함
+// component는 HTML을 반환하는 함수
+// JSX : JS를 확장한 문법, JS 안에 HTML과 JS의 모든 기능 포함 - react 개념
+ReactDOM.render(<App />, document.getElementById('root'));
